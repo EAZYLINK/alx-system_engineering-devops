0240d11628b8543b96c238cca3bc05a373d68f6d
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-03-27 05:24:13 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,39 +1,11 @@
-# using puppet to make changes to the default ssh config file
-# so that one can connect to a server without typing a password
+#configure ssh config
 
-include stdlib
-
-file_line { 'SSH Private Key':
-    path               => '/etc/ssh/ssh_config',
-    line               => 'IndentifyFile ~/.ssh/school',
-    match              => '^[#]+[\s]*(?i)IdentifyFile[\s]+~/.ssh/school',
-    replace            => true,
-    append_on_no_match => true
+file_line{'Turn off passwd auth':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
 }
 
-# Regex match explaination
-#
-# ^ beginning of the line
-# [#]* at least one hash character
-# [\s]* zero or more white space characters
-# (?i)IdentifyFile case insensitive "IdentifyFile"
-# [\s]+ at least one white space character
-# ~/.ssh/id_rsa The ssh private key file path we want to replace
-
-file_line { 'Deny Password Auth':
-     path                   => '/etc/ssh/ssh_config',
-     line                   => 'PasswordAuthentication no',
-     match                  => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+~/.ssh/school',
-     replace                => true,
-     append_on_no_match     => true
+file_line{'Declare identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
 }
-
-# Regex match explaination
-#
-# ^ beginning of the line
-# [#]* at least one hash character
-# [\s]* zero or more white space characters
-# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
-# [\s]+ at least one white space character
-# ~/.ssh/id_rsa The ssh private key file path we want to replace
-# $ end of the line
