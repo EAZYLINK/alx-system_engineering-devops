0e890f4f7e3d299c913181c24bc1d9979a5657c5
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-03-29 07:18:57 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,20 @@
-# Setup new ubuntu server with nginx
+#setup nginx
 
-exec { 'update system':
-    commad => '/usr/bin/apt-get update',
+package {
+    'nginx':
+    ensure => installed,
 }
 
-package { 'nginx':
-    ensure  => 'install',
-    require => Exec['update system']
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
-file { '/var/www/html/index.html':
-    content => 'Hello World!'
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
 }
 
-exec { 'redirect_me':
-    comman  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
-}
-
-service { 'nginx':
-    ensure  => running,
-    require => Package['nginx]
+service {'nginx':
+    ensure => running,
 }
